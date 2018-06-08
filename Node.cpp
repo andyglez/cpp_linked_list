c7460852d2ee17bde86252ab0623f656f0e03c11
Andy Gonzalez <aglez9538@gmail.com>
2018-06-08 15:55:58 -0400

    Defining generic nodes

---
+++ Node.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Andy on 6/8/2018.
+//
+
+template  <class T>
+class Node{
+public:
+    Node(T value){
+
+    }
+};
--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include "Node.cpp"
 
 using namespace std;
 
 int main() {
+    Node<int> *n = new Node<int>(5);
     cout << "Hello, World!" << endl;
     return 0;
 }
